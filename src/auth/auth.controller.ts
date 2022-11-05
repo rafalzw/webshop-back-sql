d2b6be03da3441641064db93cd2724c4cb913ff7
Rafal Zw <zwierz.rafal@gmail.com>
2022-11-05 22:12:44 +0100

    Added userLogin method.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthLoginDto } from './dto/auth-login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async userLogin(
+    @Body() req: AuthLoginDto,
+    @Res() res: Response,
+  ): Promise<any> {
+    return this.authService.login(req, res);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { Response } from 'express';
+import * as bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { User } from '../user/user.entity';
+import { JwtPayload } from './jwt.strategy';
+import { sign } from 'jsonwebtoken';
+import { config } from '../config/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  protected createToken(currentTokenId: string): {
+    accessToken: string;
+    expiresIn: number;
+  } {
+    const payload: JwtPayload = { id: currentTokenId };
+    const expiresIn = 60 * 60 * 24;
+    const accessToken = sign(payload, config.jwtSecret, {
+      expiresIn,
+    });
+    return {
+      accessToken,
+      expiresIn,
+    };
+  }
+
+  protected async generateToken(user: User): Promise<string> {
+    let token;
+    let userWithThisToken = null;
+    do {
+      token = uuid();
+      userWithThisToken = await User.findOne({
+        where: { currentTokenId: token },
+      });
+    } while (!!userWithThisToken);
+    user.currentTokenId = token;
+    await user.save();
+
+    return token;
+  }
+
+  async login(req: AuthLoginDto, res: Response): Promise<any> {
+    try {
+      const user = await User.findOne({
+        where: {
+          username: req.username,
+        },
+      });
+
+      if (user && (await bcrypt.compare(req.password, user.password))) {
+        const token = this.createToken(await this.generateToken(user));
+
+        return res
+          .cookie('jwt', token.accessToken, {
+            secure: true,
+            httpOnly: true,
+          })
+          .json({
+            isSuccess: true,
+            role: user.role,
+            firstName: user.firstName,
+            lastName: user.lastName,
+          });
+      }
+
+      return res.json({ error: 'Invalid login data!' });
+    } catch (e) {
+      return res.json({ error: e.message });
+    }
+  }
+}
