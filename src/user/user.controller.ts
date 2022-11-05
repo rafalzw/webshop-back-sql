115a1a0240223e8a4df52fd8b27d2506addde28d
Rafal Zw <zwierz.rafal@gmail.com>
2022-11-05 18:17:21 +0100

    Implemented a register method for UserServices.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterDto } from './dto/register.dto';
+import { RegisterUserResponse } from '../types/user';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(@Inject(UserService) private readonly userService: UserService) {}
+
+  @Post('/register')
+  registerUser(@Body() user: RegisterDto): Promise<RegisterUserResponse> {
+    return this.userService.register(user);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { RegisterUserResponse } from '../types/user';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async register(user: RegisterDto): Promise<RegisterUserResponse> {
+    const { firstName, lastName, username, email, password } = user;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User();
+
+    newUser.firstName = firstName;
+    newUser.lastName = lastName;
+    newUser.username = username;
+    newUser.email = email;
+    newUser.password = hashedPassword;
+
+    await newUser.save();
+
+    return {
+      isSuccess: true,
+      user: { firstName, lastName, username, email },
+    };
+  }
+}
